3f7d7f3b50a9651a4529245f4db88f3ebd3cbcf4
andres2204 <medinaandres2204@gmail.com>
2026-05-11 00:28:10 -0500

    update Commands struct name to Command

--- src/tasks/command.rs
+++ src/tasks/command.rs
@@ -2,10 +2,10 @@ use defmt::{info, error};
 use embassy_executor::task;
 use embassy_sync::pubsub::DynSubscriber;
 use embassy_time::{Timer, Duration};
-use crate::events::Commands;
+use crate::events::Command;
 
 #[task]
-pub async fn catch_commands(mut command_channel: DynSubscriber<'static, Commands>) {
+pub async fn catch_commands(mut command_channel: DynSubscriber<'static, Command>) {
     loop {
         let command = command_channel.try_next_message_pure();
         if let Some(c) = command {
