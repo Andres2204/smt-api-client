65a96afe0a46f30afe11ff122b963efa3a1cb757
andres2204 <medinaandres2204@gmail.com>
2026-05-11 00:22:59 -0500

    simple command task for testing

---
+++ src/tasks/command.rs
@@ -0,0 +1,19 @@
+use defmt::{info, error};
+use embassy_executor::task;
+use embassy_sync::pubsub::DynSubscriber;
+use embassy_time::{Timer, Duration};
+use crate::events::Commands;
+
+#[task]
+pub async fn catch_commands(mut command_channel: DynSubscriber<'static, Commands>) {
+    loop {
+        let command = command_channel.try_next_message_pure();
+        if let Some(c) = command {
+            info!("Received command {}", c);
+        } else {
+            error!("no commands");
+        }
+
+        Timer::after(Duration::from_secs(2)).await;
+    }
+}
